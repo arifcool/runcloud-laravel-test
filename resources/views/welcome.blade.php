a78f82c2f3040d147396d4ed674d57727408bbc6
arifcool <arif@coolcode.my>
2017-05-17 02:43:08 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Deployment With RunCloud
+                    Mari Guna RunCloud
                 </div>
 
                 <div class="links">
